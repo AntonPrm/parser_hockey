2bba084cced031ad4f33ef202aacdd087350c31c
Anton <kondakow90@gmail.com>
2023-05-28 12:13:06 +0500

    test commit and push in main branch

--- parser_01.py
+++ parser_01.py
@@ -1,1 +1,2 @@
 page_links = ['https://www.khl.ru/calendar/1097/00/', 'https://www.khl.ru/calendar/1045/00/', 'https://www.khl.ru/calendar/851/00/', 'https://www.khl.ru/calendar/671/00/']
+#push in main branch
